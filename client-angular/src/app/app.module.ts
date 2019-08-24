3823b29253ac9204889af8b30f11ae6841bdf5fc
Andrew Snyder <asnyder0328@gmail.com>
2019-08-24 12:56:32 -0500

    Removed un-used code from starter project

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ProjectDisplayComponent } from './project-display/project-display.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ProjectDisplayComponent, RegisterComponent, LoginComponent],
+  declarations: [AppComponent, RegisterComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
