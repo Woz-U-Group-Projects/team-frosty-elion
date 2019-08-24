69b508dced2655166280a16bad35891d2fb623ad
Andrew Snyder <asnyder0328@gmail.com>
2019-08-23 20:26:16 -0500

    Created Landing Page
    Created Landing Page with Links

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProjectDisplayComponent } from './project-display/project-display.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ProjectDisplayComponent],
+  declarations: [AppComponent, ProjectDisplayComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
