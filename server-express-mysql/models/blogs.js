8a797758787ad90a257edb85c3bd1b0e99669b1b
Andrew Snyder <Asnyder0328@gmail.com>
2019-09-13 15:32:49 -0500

    Minor Changes

---
+++ server-express-mysql/models/blogs.js
@@ -0,0 +1,33 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('blogs', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true
+    },
+    postId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    post: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    tableName: 'blogs'
+  });
+};
--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const mysql = require('mysql2');
+var models = require('../models');
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+/* GET blog posts */
+router.get("/blogs", function(req, res, next) {
+  models.blogs
+    .findAll({include: [{ table: tables.title}]})
+    .then(blogsFound => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(blogsFound));
+    });  
 });
 
 module.exports = router;
