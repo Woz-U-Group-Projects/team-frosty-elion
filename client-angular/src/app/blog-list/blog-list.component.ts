57e2760b32b8c28674ba406af0d2ba83dffa9233
AJ Bernabeo <ajbernabeo@gmail.com>
2019-09-15 13:23:27 -0700

    edited "addBlog" and MySQL

--- client-angular/src/app/blog-list/blog-list.component.ts
+++ client-angular/src/app/blog-list/blog-list.component.ts
@@ -12,22 +12,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BlogListComponent implements OnInit {
   blogs: Blog[];
-  private blogRoute = 'http://localhost:3000/blogs';
   constructor(private blogService: BlogService, private router: Router,private  http:HttpClient) { }
 
-  getBlogs(){
-    this.http.get<Blog[]>(this.blogRoute).subscribe(blogs=>{
-      this.blogs = blogs;
-    });
-  }
-
-  deleteBlog(id:number) : void {
-    console.log("delete button works!")
-    this.blogService.deleteBlog(id).subscribe(blogs => this.getBlogs());
-  }
 
   ngOnInit() {
-    this.getBlogs();
+    this.blogService.getPost().subscribe(blogs => this.blogs = blogs);
   }
 
 }
